1e16e24015d775236e7967b01f6660fe78b7e75c
Szabolcs Horvát <szhorvat@gmail.com>
2015-08-28 18:28:21 +0200

    Restructure package to allow private loading by other packages

---
+++ LTemplate/LTemplatePrivate.m
@@ -0,0 +1,9 @@
+(* Mathematica Package *)
+
+(* Load this file with Get["LTemplate`LTemplatePrivate`"] to be used privately by other packages. *)
+
+BeginPackage["`LTemplate`", {"SymbolicC`", "CCodeGenerator`", "CCompilerDriver`"}]
+
+Get["LTemplate`LTemplateInner`"]
+
+EndPackage[]
