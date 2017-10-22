8afbd1913b5d9c2b76ba9e44c7abe65591b2c78e
Szabolcs Horvát <szhorvat@gmail.com>
2017-10-22 15:24:45 +0200

    Example: Expand SparseArray

--- LTemplate/Documentation/Examples/SparseArray/Sparse.h
+++ LTemplate/Documentation/Examples/SparseArray/Sparse.h
@@ -1,8 +1,11 @@
 
 #include <LTemplate.h>
+#include <iomanip>
 
 class Sparse {
-public:    
+public:
+    // ACCESSING PROPERTIES
+
     mma::RealTensorRef explicitValues(mma::SparseArrayRef<double> sa) {
         // The Tensor returned by .explicitValues() is part of the SparseArray data structure,
         // and should not be modified. Therefore we .clone() it before returning it to Mathematica.
@@ -17,11 +20,11 @@ public:
     }
 
     mma::IntTensorRef rowPointers(mma::SparseArrayRef<double> sa) {
-        return sa.rowPointers();
+        return sa.rowPointers().clone();
     }
 
     mma::IntTensorRef columnIndices(mma::SparseArrayRef<double> sa) {
-        return sa.columnIndices();
+        return sa.columnIndices().clone();
     }
 
     // Find the rank of a sparse array.
@@ -37,6 +40,9 @@ public:
         return sa.implicitValue();
     }
 
+
+    // DENSE <-> SPARSE CONVERSIONS
+
     // Convert sparse arrays to dense ones.
     mma::RealTensorRef toDense(mma::SparseArrayRef<double> sa) {
         return sa.toTensor();
@@ -47,12 +53,66 @@ public:
         return t.toSparseArray();
     }
 
+
+    // ELEMENT ACCESS
+
     // Iterate through all index-pairs of a matrix and print the corresponding element.
     void printAll(mma::SparseMatrixRef<double> sm) {
         mma::mout << "Rows: " << sm.rows() << ", Cols: " << sm.cols() << std::endl;
-        for (int i=0; i < sm.rows(); ++i)
-            for (int j=0; j < sm.cols(); ++j) {
-                mma::mout << "(" << i << ", " << j << "): " << sm(i,j) << std::endl;
-            }
+        for (int i=0; i < sm.rows(); ++i) {
+            for (int j=0; j < sm.cols(); ++j)
+                mma::mout << std::setw(9) << sm(i,j);
+            mma::mout << '\n';
+        }
+        mma::mout << std::endl;
+    }
+
+
+    // CREATING SPARSE ARRAYS FROM SCRATCH
+
+    // Create a 5-by-6 sparse array by explicitly specifying the stored elements
+    mma::SparseMatrixRef<double> createMatrixDemo() {
+        // The positions matrix must me n-by-2 in size
+        // Positions indices start at 1, as in Mathematica, not in 0, as in C++
+        auto pos = mma::makeMatrix<mint>({{1,1}, {3,4}, {2,3}});
+
+        // The explicit value array
+        auto vals = mma::makeVector<double>({1.0, 2.5, 3.6});
+
+        auto sm = mma::makeSparseMatrix(pos, vals, 5, 6);
+
+        // The Tensors we used for constructing the sparse matrix must be explicitly freed
+        pos.free();
+        vals.free();
+
+        return sm;
+    }
+
+    // Create a 3D sparse array, analogously to createMatrixDemo()
+    mma::SparseArrayRef<double> createCubeDemo() {
+        auto pos  = mma::makeMatrix<mint>({{1,2,1}, {3,2,1}});
+        auto vals = mma::makeVector<double>({1.5,3.2});
+        auto dims = mma::makeVector<mint>({3,3,3});
+
+        auto sa = mma::makeSparseArray(pos, vals, dims);
+
+        pos.free();
+        vals.free();
+        dims.free();
+
+        return sa;
+    }
+
+    // Create a 3-by-4 all-zero SparseArray
+    mma::SparseMatrixRef<double> createZeroMatrixDemo() {
+        auto pos  = mma::makeMatrix<mint>(0,2);
+        auto vals = mma::makeVector<double>(0);
+
+        auto sa = mma::makeSparseMatrix(pos, vals, 3, 4);
+
+        pos.free();
+        vals.free();
+
+        return sa;
     }
 };
