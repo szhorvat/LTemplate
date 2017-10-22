6a02e18a4483eceea670ddae519bf14d0b845a86
Szabolcs Horvát <szhorvat@gmail.com>
2017-10-22 15:44:45 +0200

    Examples: Armadillo

--- LTemplate/Documentation/Examples/Armadillo/Arma.h
+++ LTemplate/Documentation/Examples/Armadillo/Arma.h
@@ -5,10 +5,13 @@
 #define ARMA_CERR_STREAM mma::mout
 #include <armadillo>
 
-// Armadillo stores matrices in column major order, while Mathematica uses row-major order.
-// It is thus fastest to convert an MTensor to its transposed Armadillo equivalent.
+// We start by defining conversion functions to/from Armadillo's basic datatypes:
+// matrices, column vectors and sparse matrices
 
-// Convert from Mathematica matrix to the Armadillo matrix without copying any data.
+// Armadillo stores matrices in column major order, while Mathematica uses row-major order.
+// It is thus fastest to convert an MTensor to/from its transposed Armadillo equivalent.
+
+// Convert from Mathematica matrix to the transposed Armadillo equivalent without copying any data.
 template<typename T>
 arma::Mat<T> toArmaTransposed(mma::MatrixRef<T> m) {
     // wrap MTensor with arma::mat without copying
@@ -16,10 +19,10 @@ arma::Mat<T> toArmaTransposed(mma::MatrixRef<T> m) {
     return arma::Mat<T>(m.data(), m.cols(), m.rows(), false /* do not copy */, false /* until resized */);
 }
 
-// Convert from Armadillo matrix to Mathematica matrix.
+// Convert from Armadillo matrix to transposed Mathematica matrix.
 // This time the data must be copied.
 template<typename T>
-mma::TensorRef<T> fromArmaTransposed(arma::Mat<T> m) {
+mma::TensorRef<T> fromArmaTransposed(const arma::Mat<T> &m) {
     return mma::makeMatrix<T>(m.n_cols, m.n_rows, m.memptr());
 }
 
@@ -31,7 +34,7 @@ arma::Col<T> toArmaVec(mma::TensorRef<T> v) {
 
 // Convert from Armadillo column vector to Mathematica vector with copying
 template<typename T>
-mma::TensorRef<T> fromArmaVec(arma::Col<T> v) {
+mma::TensorRef<T> fromArmaVec(const arma::Col<T> &v) {
     return mma::makeVector<T>(v.size(), v.memptr());
 }
 
@@ -47,6 +50,30 @@ arma::SpMat<T> toArmaSparseTransposed(mma::SparseMatrixRef<T> sm) {
            );
 }
 
+template<typename T>
+mma::SparseMatrixRef<T> fromArmaSparse(const arma::SpMat<T> &am) {
+    auto pos  = mma::makeMatrix<mint>(am.n_nonzero, 2);
+    auto vals = mma::makeVector<double>(am.n_nonzero);
+
+    // iterate through all explicitly stored elements in the Armadillo sparse matrix
+    mint i = 0;
+    for (typename arma::SpMat<T>::const_iterator it = am.begin();
+         it != am.end();
+         ++it, ++i)
+    {
+        vals[i] = *it;
+        pos(i,0) = it.row() + 1; // convert 0-based index to 1-based
+        pos(i,1) = it.col() + 1;
+    }
+
+    auto mm = mma::makeSparseMatrix(pos, vals, am.n_rows, am.n_cols);
+
+    pos.free();
+    vals.free();
+
+    return mm;
+}
+
 class Arma {
 public:
     mma::RealMatrixRef inv(mma::RealMatrixRef mat) {
@@ -56,6 +83,10 @@ public:
 
     mma::ComplexTensorRef eigs(mma::SparseMatrixRef<double> sm, mint k) {
         return fromArmaVec<mma::complex_t>(arma::eigs_gen(toArmaSparseTransposed(sm), k));
+    }
+
+    mma::SparseMatrixRef<double> sprandu(mint i, mint j, double dens) {
+        return fromArmaSparse(arma::sprandu<arma::sp_mat>(i, j, dens));
     }
         
     void print(mma::RealMatrixRef mat) {
